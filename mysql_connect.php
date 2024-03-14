9c24c4ae6fab35249e6a1ee8a6ddfcbc6b7ab36e
SalmanMohammed2001 <salmannisthar123@gmail.com>
2024-03-15 02:40:29 +0530

    setup mysql connection;

---
+++ mysql_connect.php
@@ -0,0 +1,8 @@
+
+<?php
+   $con= new mysqli("localhost","root","1234","customer_crud");
+   if($con){
+       echo "connected";
+   }else{
+       die(mysqli_errno($con));   /*die- destroy */
+   }
